7400de4204a3148b2c4a851750e0e4107a79581f
Mary-Kate James <marykatj@gmail.com>
2018-05-24 18:23:29 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+  arr.sort_by { |a| a.split("").map{ |letter| ESPERANTO_ALPHABET.index(letter) } }
 end
